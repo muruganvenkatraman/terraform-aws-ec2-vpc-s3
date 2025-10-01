a2780da3cf804714a7bb2d0ce13fa3f0511d4119
muruganvenkatraman <muruganvenkat0007@gmail.com>
2025-10-01 23:01:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,29 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1" # Mumbai
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "bucket_name" {
+  description = "Unique S3 bucket name"
+  type        = string
+}
 
