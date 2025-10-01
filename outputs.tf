494223433b3e27ffe824b562079ab8e1a7fdf939
muruganvenkatraman <muruganvenkat0007@gmail.com>
2025-10-01 23:02:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,16 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.main.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.bucket.bucket
+}
 
